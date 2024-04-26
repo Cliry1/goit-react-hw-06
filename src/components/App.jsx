c23672dcb837868d3ebd80ba99dfe65bc5c8958d
PRO100 Cliry <128536194+Cliry1@users.noreply.github.com>
2024-04-26 11:41:00 +0300

    new

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,19 @@
 import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 import SearchBox from './SearchBox/SearchBox';
-import { PersistGate } from 'redux-persist/integration/react'
-import { Provider } from 'react-redux';
-import { persistor, store } from '../redux/store';
+
 export default function App() {
  
 
 
   return (
     <>    
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
+
         <h1>Phonebook</h1>
         <ContactForm />
         <SearchBox />
         <ContactList />
-      </PersistGate>
-    </Provider>
+
     </>
 
   )
--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,17 +1,18 @@
 import css from "./SearchBox.module.css"
-import { useDispatch } from "react-redux"
-import { changeFilter } from "../../redux/filtersSlice";
+import { useDispatch, useSelector } from "react-redux"
+import { changeFilter, selectNameFilter } from "../../redux/filtersSlice";
 export default function SearchBox() {
 
   const dispatch = useDispatch();
   const handleChange = (e)=>{
     dispatch(changeFilter(e.target.value))
   }
+  const filter = useSelector(selectNameFilter);
 
   return (
   <div className={css.container}>
       <p className={css.text}>Find contacts by name</p>
-      <input className={css.input} type="text" name="filter"  onChange={handleChange}></input>
+      <input className={css.input} value={filter} type="text" name="filter"  onChange={handleChange}></input>
   </div>
   )
 }
